8ba6266cd62821805922f1c489098dbab3e3bc0d
hien <hien793041@gmail.com>
2024-07-17 19:03:38 +0700

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -7,19 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use('/artifacts', express.static(path.join(__dirname, 'artifacts')));
-app.use('/', (req, res) => {
-  res.send("server is running")
-});
-app.get('/api/spagghetiDex', async (req, res) => {
-  try {
-    const filePath = path.join(__dirname, 'artifacts', 'spagghetiDEX.json');
-    // You can use fs.readFile to read the file content if needed
-    res.sendFile(filePath);
-  } catch (error) {
-    console.error('Error fetching spagghetiDex.json:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
